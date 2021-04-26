a008f31a08f6803fe7d2a74ad01b2d7c70225539
Agustina <agmontserratgarcia@gmail.com>
2021-04-26 01:00:43 -0300

    Cambie las urls como lo hace el profe en el curso

--- Combi19/urls.py
+++ Combi19/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from Combi19App import views
+from django.urls import path, include  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="Home"),
-    path('', views.insumos, name="Insumos"),
-    path('', views.pasajes, name="Pasajes")
+    path('', include('Combi19App.urls'))
 ]
