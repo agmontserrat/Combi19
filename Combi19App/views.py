69d3001bf4c056e0ec21a61ae9b0463296725ba4
Agustina <agmontserratgarcia@gmail.com>
2021-04-26 01:02:19 -0300

    Agregué los html asi que ahora los renderiza en vez de mostrar texto

--- Combi19App/views.py
+++ Combi19App/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+# Aca creamos nuestras vistas.
 def home (request):
-    return HttpResponse("Home")
+    return render(request, "Combi19App/home.html")
 
 def pasajes (request):
-    return HttpResponse("Pasajes")
+    return render(request, "Combi19App/pasajes.html")
+
 
 def insumos (request):
-    return HttpResponse("Insumos")
+    return render(request, "Combi19App/insumos.html")
+
