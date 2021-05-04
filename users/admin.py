739cb7107e2ce0b27280bd68f33c9455eb347b24
Agustina <agmontserratgarcia@gmail.com>
2021-05-03 22:27:43 -0300

    Admin funciona

--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from users.models import Account
 
+
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'last_login', 'is_admin', 'is_staff')
+    list_display = ('email', 'last_login', 'first_name', 'is_admin', 'is_staff')
     search_fields = ('email',)
     readonly_fields = ('id', 'last_login')
 
