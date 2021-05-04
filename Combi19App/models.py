c7ad99a34c5e7617c18cdfd1ed5b7d455fb21120
Agustina <agmontserratgarcia@gmail.com>
2021-05-03 22:26:05 -0300

    Se agregaron las tablas al admin

--- Combi19App/models.py
+++ Combi19App/models.py
@@ -7,10 +7,17 @@ class Vehiculo(models.Model):
     capacidad = models.IntegerField()
     modelo = models.IntegerField()
 
+    def __str__(self):
+        return self.patente
+
 class Insumo(models.Model):
     nombre = models.CharField(max_length=15)
     descripcion = models.CharField(max_length=30)
     precio = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
 
 class Pasaje():
     pass
@@ -18,8 +25,12 @@ class Pasaje():
 class Viaje(models.Model):
     fecha = models.DateField()
 
+
 class Lugar(models.Model):
     nombre = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nombre
     
 
 class Ruta(models.Model):
