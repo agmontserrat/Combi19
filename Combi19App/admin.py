5e8bd52136f656c7548840ba6358d77c2f0bb80e
Agustina <agmontserratgarcia@gmail.com>
2021-05-03 22:26:32 -0300

    Se agregaron las tablas al admin

--- Combi19App/admin.py
+++ Combi19App/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Vehiculo, Insumo, Viaje, Lugar
 
 # Register your models here.
+
+admin.site.register(Vehiculo)
+admin.site.register(Insumo)
+admin.site.register(Viaje)
+admin.site.register(Lugar)
