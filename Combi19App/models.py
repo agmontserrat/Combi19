b32552bffd6500cad6b04924d53c907d74d4c6be
antonella <antonellagarea37@gmail.com>
2021-05-05 10:24:53 -0300

    AHORA INSUMOS CON IMAGENESSS

--- Combi19App/models.py
+++ Combi19App/models.py
@@ -14,6 +14,7 @@ class Insumo(models.Model):
     nombre = models.CharField(max_length=15)
     descripcion = models.CharField(max_length=30)
     precio = models.IntegerField()
+    imagen = models.ImageField(upload_to="insumos", null=True)
 
     def __str__(self):
         return self.nombre
