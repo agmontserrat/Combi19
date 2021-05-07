4820614bc79147b1b4cc6b68df23d7120be04beb
manutonelli <mtonelli91@gmail.com>
2021-05-07 17:15:18 -0300

    actualice index

--- Combi19App/urls.py
+++ Combi19App/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from Combi19App import views
 
 urlpatterns = [
-    path('', views.home, name="Home"),
+    path('home', views.home, name="Home"),
     path('insumos', views.insumos, name="Insumos"),
     path('pasajes', views.pasajes, name="Pasajes"),
     path('registro', views.registro, name="Registro"),
-    path('index', views.index, name="Index")
+    path('', views.index, name="Index")
     
 ]
