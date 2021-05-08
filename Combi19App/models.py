adc8e858787a71af3441dcf8b44a8eb48107f747
Antonella Garea <antonellagarea37@gmail.com>
2021-05-08 11:39:50 -0300

    SOLUCIONADO RUTASSSS

--- Combi19App/models.py
+++ Combi19App/models.py
@@ -32,15 +32,16 @@ class Viaje(models.Model):
 
 class Lugar(models.Model):
     nombre = models.CharField(max_length=15)
+    verbose_name_plural='Lugares'
 
     def __str__(self):
         return self.nombre
 
 
 class Ruta(models.Model):
-    choices = [(lugar.id, lugar.nombre) for lugar in Lugar.objects.all()]
-    origen = (models.IntegerField(null=False, blank=False, choices=choices))
-    destino = models.IntegerField(null=False, blank=False, choices=choices)
+    choices = [(lugar.nombre, lugar.nombre) for lugar in Lugar.objects.all()]
+    origen = (models.CharField(max_length=15,null=False, blank=False, choices=choices))
+    destino = (models.CharField(max_length=15,null=False, blank=False, choices=choices))
     km = models.IntegerField()
     espacio = ' --> '
 
