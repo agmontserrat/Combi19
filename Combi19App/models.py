bc4fadef878267981021196ee18093f206550fef
Antonella Garea <antonellagarea37@gmail.com>
2021-05-08 15:12:15 -0300

    Viaje error en add

--- Combi19App/models.py
+++ Combi19App/models.py
@@ -37,18 +37,18 @@ class Ruta(models.Model):
     origen = (models.CharField(max_length=15,null=False, blank=False, choices=choices))
     destino = (models.CharField(max_length=15,null=False, blank=False, choices=choices))
     km = models.IntegerField()
-    espacio = ' --> '
-    nombre = str(origen)+str(espacio)+str(destino)
-
     def __str__(self):
-        return f'{self.origen}{self.espacio}{self.destino}'
+        return f'Origen: {self.origen} --> Destino: {self.destino}'
 
 class Viaje(models.Model):
     fecha = models.DateField()
     ruta = models.ForeignKey(Ruta, default=None, on_delete=models.CASCADE)
+    #choicesC = [(combi.patente, combi.patente) for combi in Vehiculo.objects.all()]
+    #combi = (models.CharField(max_length=15,null=False, blank=False, choices=choicesC))
+    combi = models.ForeignKey(Vehiculo, default=None, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'Fecha: {self.fecha} - Ruta: {self.ruta} - Combi:{combi} '
     
-    # combi = 
-
 class Combi():
     pass
 
