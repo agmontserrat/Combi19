072a91506e26bf672a7e6c5bbd4c47b37684821f
Agustina <agmontserratgarcia@gmail.com>
2021-05-09 20:36:25 -0300

    BD arreglada. Añadir Viaje solucionado

--- Combi19App/models.py
+++ Combi19App/models.py
@@ -34,23 +34,23 @@ class Lugar(models.Model):
 
 class Ruta(models.Model):
     choices = [(lugar.nombre, lugar.nombre) for lugar in Lugar.objects.all()]
-    origen = (models.CharField(max_length=15,null=False, blank=False, choices=choices))
-    destino = (models.CharField(max_length=15,null=False, blank=False, choices=choices))
+    origen = (models.CharField(max_length=15, null=False, blank=False, choices=choices))
+    destino = (models.CharField(max_length=15, null=False, blank=False, choices=choices))
     km = models.IntegerField()
     def __str__(self):
         return f'Origen: {self.origen} --> Destino: {self.destino}'
 
 class Viaje(models.Model):
-    fecha = models.DateField()
-    ruta = models.ForeignKey(Ruta, default=None, on_delete=models.CASCADE)
     #choicesC = [(combi.patente, combi.patente) for combi in Vehiculo.objects.all()]
     #combi = (models.CharField(max_length=15,null=False, blank=False, choices=choicesC))
-    combi = models.ForeignKey(Vehiculo, default=None, on_delete=models.CASCADE)
-    def __str__(self):
-        return f'Fecha: {self.fecha} - Ruta: {self.ruta} - Combi:{combi} '
+
+    fecha         =   models.DateTimeField(blank=True, null=True)
+    ruta          =   models.ForeignKey(Ruta, default=None, on_delete=models.CASCADE)
+    combi         =   models.ForeignKey(Vehiculo, blank=True, null=True, on_delete=models.CASCADE)
     
-class Combi():
-    pass
+    def __str__(self):
+        return f'Fecha: {self.fecha} - Ruta: {self.ruta} - Combi:{self.combi} '
+
 
 class Testeo():
     pass
