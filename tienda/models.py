6fff0c7945cea8692bfb3496b8f871a8826d5824
Agustina <agmontserratgarcia@gmail.com>
2021-06-21 09:53:59 -0300

    Todas las correcciones de Demo 2 terminadas

--- tienda/models.py
+++ tienda/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
 class Insumo(models.Model):
     nombre        = models.CharField(max_length=30)
     descripcion   = models.CharField(max_length=50)
-    precio        = models.IntegerField()
+    precio        = models.IntegerField(validators=[MinValueValidator(0)])
     imagen        = models.ImageField(upload_to="insumos", null=True)
-    cantidad      = models.IntegerField(blank=True, null=True)
+    cantidad      = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(0)])
     created       = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated       = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
